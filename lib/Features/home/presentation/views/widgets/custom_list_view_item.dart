19615aac0ae2dd05b79b70354d5862a103613090
Afraym Herz <afraym100herz@gmail.com>
2024-09-26 00:10:18 +0300

    create Feature list view item with specific width

---
+++ lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -0,0 +1,23 @@
+import 'package:bookly_app/Features/home/presentation/views/widgets/custom_item.dart';
+import 'package:flutter/material.dart';
+
+class FeatureListViewItem extends StatelessWidget {
+  const FeatureListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: CustomItem(),
+          ) ;
+        } ,
+        
+        ),
+    );
+  }
+}
