a1ce045587982d2a42abb5671ac1c3ff9829b082
Afraym Herz <afraym100herz@gmail.com>
2024-10-02 23:54:21 +0300

    create home repo impl and add impl to methods

---
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,42 @@
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/core/utils/api_services.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  
+  final ApiService apiService ;
+
+  HomeRepoImpl({required this.apiService});
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() async {
+    try {
+      var data = await apiService.get(endPoint: 'volumes?Filtering=free-ebooks&Sorting=newest &q=computer science') ;
+
+      List<BookModel> books =[] ;
+      for (var book in data['items']) {
+        books.add(BookModel.fromJson(book)); 
+      }
+    return right(books) ;
+    } catch (e) {
+      return left(ServerFailure()) ;
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiService.get(endPoint: 'volumes?Filtering=free-ebooks&q=subject:Programming') ;
+
+      List<BookModel> books =[] ;
+      for (var book in data['items']) {
+        books.add(BookModel.fromJson(book)); 
+      }
+    return right(books) ;
+    } catch (e) {
+      return left(ServerFailure()) ;
+    }
+  }
+}
