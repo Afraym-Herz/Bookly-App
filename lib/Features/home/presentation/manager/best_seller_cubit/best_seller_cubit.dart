849c2f7f3ebd6552c6d9c826fff0be57aa5a0b64
Afraym Herz <afraym100herz@gmail.com>
2024-10-03 13:53:57 +0300

    create best seller cubit and states

---
+++ lib/Features/home/presentation/manager/best_seller_cubit/best_seller_cubit.dart
@@ -0,0 +1,26 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../../data/models/book_model/book_model.dart';
+
+part 'best_seller_state.dart';
+
+class BestSellerCubit extends Cubit<BestSellerBooksState> {
+  BestSellerCubit(this.homeRepo) : super(BestSellerBookInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(BestSellerBooksLoading());
+
+    var result = await homeRepo.fetchBestSellerBooks();
+
+    result.fold((failure) {
+      emit(BestSellerBooksFailure(errMessage: failure.errMessage));
+    }, (books) {
+      emit(BestSellerBooksSuccess(books: books));
+    });
+  }
+
+}
